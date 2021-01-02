e76220c9a5b6337cf3c42c83cb849b4275f8cd24
kkalkanis <76869108+kkalkanis@users.noreply.github.com>
2021-01-02 16:06:01 +0200

    Metrics 0.1

--- src/java/Model/Cidoc_1.java
+++ src/java/Model/Cidoc_1.java
@@ -7,7 +7,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
-
 /**
  *
  * @author Tasos
@@ -18,14 +17,23 @@ public class Cidoc_1 {
     @Context
     private UriInfo context;
     public String Jsonstring;
-
+    
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getJson() throws IOException {
-               
-        cidoc.setQuerySpaql("select distinct ?Concept where {[] a ?Concept} LIMIT 100"); //Sparql query
+    public String getJson() throws IOException { 
+        cidoc.setQuerySpaql("SELECT (COUNT(DISTINCT ?class) as ?countClass)\n" +"WHERE {\n" +"{\n" +"[] rdf:type ?class\n" +"}UNION{\n" +"?class rdf:type owl:Class\n" +"}UNION{\n" +"?class rdf:type rdfs:Class\n" +"}\n" +"}"); //Sparql query
         cidoc.setConnection(); //Get the data and write them in String with json format
-
+        
+        
+        cidoc.setQuerySpaql("SELECT (COUNT(DISTINCT ?objectProperty) as ?countObject)\n" +"WHERE {\n" +"{\n" +"[] rdf:type ?objectProperty\n" +"}UNION{\n" +"?objectProperty rdf:type owl:ObjectProperty\n" +"}UNION{\n" +"?objectProperty rdf:type rdfs:ObjectProperty\n" +"}\n" +"}");
+        cidoc.setConnection(); //Get the data and write them in String with json format
+        
+        cidoc.setQuerySpaql("SELECT (COUNT(DISTINCT ?datatypeProperty) as ?countDataType)\n" +"WHERE {\n" +"{\n" +"[] rdf:type ?datatypeProperty\n" +"}UNION{\n" +"?datatypeProperty rdf:type owl:DataTypeProperty\n" +"}UNION{\n" +"?datatypeProperty rdf:type rdfs:DataTypeProperty\n" +"}\n" +"}");
+        cidoc.setConnection(); //Get the data and write them in String with json format
+        
+        cidoc.setQuerySpaql("SELECT (COUNT(DISTINCT ?individual) as ?countIndividual)\n" +"WHERE {\n" +"{\n" +"[] rdf:type ?individual\n" +"}UNION{\n" +"?individual rdf:type owl:NamedIndividual\n" +"}UNION{\n" +"?individual rdf:type rdfs:NamedIndividual\n" +"}\n" +"}");
+        cidoc.setConnection(); //Get the data and write them in String with json format
+        
         //TODO return proper representation object
         return cidoc.getResponsestring();
     }
