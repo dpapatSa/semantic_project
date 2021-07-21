6514f752a3721d37044698ada64ab755b814dbf0
Nikos Astyrakakis <nastyrakakis@gmail.com>
2021-07-21 17:00:49 +0300

    .

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ export VUE_APP_BACKEND_PORT=9090
 export VUE_APP_BACKEND_HOST=83.212.77.24       	   
 
 
-npm i && npm run build && sudo rm -rf /var/www/html/ && sudo cp -r dist/* /var/www/html/ && sudo systemctl restart nginx
+npm i && npm run build && sudo rm -rf /var/www/html/* && sudo cp -r dist/* /var/www/html/ && sudo systemctl restart nginx
 
 cd ../
 
